eee75d47aea94293acf0285b847bce29406ef63c
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-02-20 11:37:57 -0500

    Fix switch script

--- switch.sh
+++ switch.sh
@@ -1,1 +1,4 @@
-nix build ".#$1" && ./results/activate
+
+# Where first arg is directory under machines, can be one of macos, fedora, nixos
+nix-shell -p nixUnstable --command "nix build --experimental-features 'nix-command flakes' '.#$1'"
+nix build ".#$1" && ./result/activate
